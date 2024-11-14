58d046ad75396d3c6048bb22cf8330a19bf4d9b9
Andmana <121153225+Andmana@users.noreply.github.com>
2024-11-14 14:31:50 +0700

    UI for rps

--- script.js
+++ script.js
@@ -1,72 +1,105 @@
 const choices = ["rock", "paper", "scissors"];
+const choices2 = ["✊🏿", "🖐🏻", "✌🏼"];
+let computers = 0;
+let players = 0;
+
+const playerScore = document.getElementById("PlayerScore");
+const compScore = document.getElementById("CompScore");
+const play = document.querySelector("#Play");
+const buttons = document.querySelectorAll(".selection");
+const playerSelection = document.getElementById("PlayerSelection");
+const compSelection = document.getElementById("CompSelection");
+const roundsResult = document.getElementById("RoundsResult");
+const roundsLog = document.getElementById("RoundsLog");
+const gameOver = document.getElementById("GameOver");
+const gameResult = document.getElementById("GameResult");
+
+let scores = () => {
+    playerScore.textContent = players;
+    compScore.textContent = computers;
+};
+
+play.addEventListener("click", () => {
+    const text = play.textContent;
+    if (text == "Start Game") {
+        computers = 0;
+        players = 0;
+        scores();
+        play.textContent = "Stop Game";
+        disabledAction(false);
+        gameOver.textContent = "";
+        gameResult.textContent = "";
+    } else {
+        play.textContent = "Start Game";
+        disabledAction(true);
+    }
+});
+
 function getComputerChoice() {
     let choiceIdx = Math.floor(Math.random() * choices.length);
-    return choices[choiceIdx];
+    return choiceIdx;
 }
 
-function getHumanChoice() {
-    let inputUser = prompt(
-        "Insert your choice \n1 = rock \n2 = paper \n3 = scissors"
-    );
-    // If input null cancel the game, if input not 1-3 re input
-    if (inputUser > 0 && inputUser < 4) return choices[inputUser - 1];
-    else if (inputUser == null) return null;
-    else {
-        alert(inputUser + " Invalid input \nplease input in range 1-3");
-        return getHumanChoice();
-    }
-}
-
-//Rock-Paper Scissors logic
-function playRound(humanChoice, comChoice) {
-    if (humanChoice == comChoice) return "DRAW";
-    else if (humanChoice == "rock") {
+function rpsLogic(playerChoice, comChoice) {
+    if (playerChoice == comChoice) return "DRAW";
+    else if (playerChoice == "rock") {
         if (comChoice == "paper") return "LOSE";
         else return "WIN";
-    } else if (humanChoice == "paper") {
+    } else if (playerChoice == "paper") {
         if (comChoice == "scissors") return "LOSE";
         else return "WIN";
-    } else if (humanChoice == "scissors") {
+    } else if (playerChoice == "scissors") {
         if (comChoice == "rock") return "LOSE";
         else return "WIN";
     }
 }
 
-function playGame() {
-    let humanScore = 0;
-    let comScore = 0;
-    let round = 0;
+function gameRounds(playerSelect, compSelect) {
+    playerSelect.textContent = choices2[playerSelect];
+    compSelection.text = choices2[compSelect];
 
-    // lopping until player or comp score reach 5
-    console.log("First reach score 5 is the winner");
-    do {
-        let humanSelection = getHumanChoice();
-        const comSelection = getComputerChoice();
+    const roundRslt = rpsLogic(choices[playerSelect], choices[compSelect]);
+    let testMsg;
 
-        //Game canceled when canceling user input choice
-        if (humanSelection == null) {
-            alert("game canceled");
-            break;
+    if (roundRslt === "WIN") {
+        testMsg = "beats";
+        players++;
+    } else if (roundRslt === "LOSE") {
+        testMsg = "is beaten by";
+        computers++;
+    } else if (roundRslt === "TIE") {
+        testMsg = "ties with";
+    }
+    roundsResult.textContent =
+        roundRslt === "DRAW" ? "DRAW" : "YOU " + roundRslt;
+    roundsLog.textContent = `${choices[playerSelect]} ${testMsg} ${choices[compSelect]}`;
+
+    playerSelection.textContent = choices2[playerSelect];
+    playerScore.textContent = players;
+    compScore.textContent = computers;
+    compSelection.textContent = choices2[compSelect];
+}
+
+// Add event each selection
+buttons.forEach((selection) => {
+    selection.addEventListener("click", () => {
+        const playerSelect = selection.id;
+        const compSelect = getComputerChoice();
+
+        gameRounds(playerSelect, compSelect);
+
+        if (players == 5 || computers == 5) {
+            disabledAction(true);
+            play.textContent = "Start Game";
+            gameOver.textContent = "GAME OVER";
+            gameResult.textContent =
+                players > computers ? "YOU WIN" : "YOU LOSE";
         }
+    });
+});
 
-        const roundResult = playRound(humanSelection, comSelection);
-
-        // Score increment each round
-        if (roundResult == "WIN") humanScore++;
-        else if (roundResult == "LOSE") comScore++;
-
-        // Result log
-        console.log(
-            `Rounds : ${round} \t ${humanSelection} vs ${comSelection}`
-        );
-        console.log(
-            `${roundResult}\tHuman : ${humanScore} \t Comp : ${comScore}`
-        );
-
-        if (humanScore == 5 || comScore == 5) break;
-        round++;
-    } while (true);
-
-    if (humanScore > comScore) console.log("You're a WINNERS!!!");
-    else console.log("LOOOOSSSSEEEEEEERRRRSSSSSS!!!");
+function disabledAction(isDisable) {
+    buttons.forEach((button) => {
+        button.disabled = isDisable;
+    });
 }
